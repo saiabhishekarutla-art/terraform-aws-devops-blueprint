6b1ef133992205117421b82e900c4158f444d438
abhishek arutla <saiabhishekarutla@gmail.com>
2026-03-08 18:01:22 -0400

    Add subnet, internet gateway, and route table

--- main.tf
+++ main.tf
@@ -9,3 +9,38 @@ resource "aws_vpc" "devops_vpc" {
     Name = "devops-blueprint-vpc"
   }
 }
+  resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.devops_vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "devops-blueprint-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.devops_vpc.id
+
+  tags = {
+    Name = "devops-blueprint-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.devops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "devops-blueprint-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
